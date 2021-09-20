f2fa4ef9e80608d4071102c351212163ffff40db
Félix Larose-Gervais <filedesless@gmail.com>
2021-09-20 14:26:02 -0400

    swapped icons to match comp

--- src/core/constants.py
+++ src/core/constants.py
@@ -5,12 +5,12 @@ ROLE_REACTIONS = [
     "<:ranger_soulbeast:750067385435816036>",
     "<:revenant_renegade:750067383628071012>",
     "<:warrior_berserker:750067384706007120>",
-    "<:necro_scourge:750067385448267846>",
-    "<:engineer_holosmith:750067385104597114>",
     "<:thief_daredevil:750067385062391909>",
     "<:mesmer_chronomancer:750067386207698965>",
     "<:ele_tempest:750067384961991007>",
-    "<:necro_reaper:750067384563400734>"]
+    "<:necro_scourge:750067385448267846>",
+    "<:necro_reaper:750067384563400734>",
+    "<:necro:750067383636590672>"]
 
 SUBMIT_REACTION = "\U00002705"
 DEFAULT_REACTIONS = ROLE_REACTIONS + [SUBMIT_REACTION]
